4bfd6b7aa7b339013552aef84de56889100c6f50
nainxav <nainciaa@gmail.com>
2025-06-01 21:22:59 +0700

    pertama

---
+++ src/js/app.js
@@ -0,0 +1,43 @@
+// app.js
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();              // Create an Express application
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
+
+// Middleware configuration
+app.use(express.json());            // Parse incoming JSON requests
+app.use(express.urlencoded({ extended: false }));  // Parse URL-encoded data
+app.use(cors());                    // Enable CORS for cross-origin requests
+
+app.use(express.static(path.join(__dirname, "../../public")));
+// app.use(express.static('public'));
+
+// Routes
+app.get("/", (req, res) => {
+  res.render("index", { title: "Welcome to the Server" });
+});
+
+app.get("/signin", (req, res) => {
+  res.render("signin", { title: "Welcome to the Server" });
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup", { title: "Welcome to the Server" });
+});
+
+app.get("/dashboard", (req, res) => {
+  res.render("dashboard", { title: "Welcome to the Server" });
+});
+
+
+// Start server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
